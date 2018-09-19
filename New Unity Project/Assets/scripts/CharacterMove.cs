8d1528a6dc5557108e4bc2112666f731de5dab8b
Brennon <bhar9463@gmail.com>
2018-09-19 16:18:28 -0600

    New Unity Game
    started over on the graphics and scales

--- New Unity Project/Assets/scripts/CharacterMove.cs
+++ New Unity Project/Assets/scripts/CharacterMove.cs
@@ -15,7 +15,7 @@ public class CharacterMove : MonoBehaviour {
 	public LayerMask WhatIsGround;
 	private bool Grounded;
 
-	// Use this for initialization
+	// Use this for initialization or in game startup
 	void Start () {
 		 
 	}
@@ -24,14 +24,15 @@ public class CharacterMove : MonoBehaviour {
 	void FixedUpdate () {
 		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
 	}
-	// Update is called once per frame
+	// Update is called once per frame or recycles every frame
 	void Update () {
 	
-	// This code makes the character jump
+	// This code makes the character jump 
+	// "If" is when you click a specific key to make something do a certain action
 	if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 		Jump();
 	}
-	// This code makes the character move from side to side with A/D 
+	// This code makes the character move from side to side with A/D keyboard keys
 	if(Input.GetKey (KeyCode.D)) {
 			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
