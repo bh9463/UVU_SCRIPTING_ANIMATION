21d5a0989b67e7d52ab84870757e9a161da4eafd
brennon <bhar9463@gmail.com>
2018-09-26 17:18:12 -0600

    sprites + dbl jump + velocity change + score
    added our sprites and added some new physics plus added a scoring text

--- New Unity Project/Assets/scripts/CharacterMove.cs
+++ New Unity Project/Assets/scripts/CharacterMove.cs
@@ -8,12 +8,15 @@ public class CharacterMove : MonoBehaviour {
 	// Player Movement Variables
 	public int MoveSpeed; 
 	public float JumpHeight; 
+	private bool doubleJump;
 
 	// Player grounded variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+	private float moveVelocity;
+
 
 	// Use this for initialization or in game startup
 	void Start () {
@@ -32,15 +35,30 @@ public class CharacterMove : MonoBehaviour {
 	if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 		Jump();
 	}
+
+	// double jump
+	if(Grounded)
+		doubleJump = false;
+
+	if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !Grounded){
+			Jump();
+			doubleJump = true;
+	}
+
+	moveVelocity = 0f;
+
 	// This code makes the character move from side to side with A/D keyboard keys
 	if(Input.GetKey (KeyCode.D)) {
 			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+			moveVelocity = MoveSpeed;
 	}
 	if(Input.GetKey (KeyCode.A)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+			moveVelocity = -MoveSpeed;
 	}
+
+	GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
 }
 
 public void Jump(){
