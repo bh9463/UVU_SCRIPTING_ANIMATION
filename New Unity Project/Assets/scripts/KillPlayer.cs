15e295749cbac1ab1c62a9dfebe0376c4d21555f
brennon <bhar9463@gmail.com>
2018-10-10 17:16:45 -0600

    added respawn
    applied respawn particles and a respawn and death

---
+++ New Unity Project/Assets/scripts/KillPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+
+	public Level1Manager Level1Manager;
+
+	// populating it 
+	void Start () {
+		Level1Manager = FindObjectOfType <Level1Manager>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "Jimmy"){
+			Level1Manager.RespawnPlayer();
+			}
+
+		}
+	}
