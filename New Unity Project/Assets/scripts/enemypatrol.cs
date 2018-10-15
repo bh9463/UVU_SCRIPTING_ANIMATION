0d2ba9edbb064991979f1bff0471a400ae7b236e
brennon <bhar9463@gmail.com>
2018-10-15 17:18:21 -0600

    created enemy patrol
    enemy patrol back and forth created

---
+++ New Unity Project/Assets/scripts/enemypatrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemypatrol : MonoBehaviour {
+
+	// Movement Variables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	// Wall Check
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	// items in layer = LayerMask
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	// Edge Check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+
+
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+		
+		// turn around when hitting the wall or the edge
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight; 
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-6f,6f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else{
+			transform.localScale = new Vector3(6f,6f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
