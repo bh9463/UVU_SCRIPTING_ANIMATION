7dbc190713550fb56932b398e89a6035ae1f357e
brennon <bhar9463@gmail.com>
2018-10-22 16:39:51 -0600

    Started on shooting
    In the middle of projectile code

---
+++ New Unity Project/Assets/scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float Speed;
+	public Rigidbody2D PC;
+
+	public GameObject EnemyDeath;
+
+	public GameObject ProjectileParticle;
+
+	public int PointsForKill;
+
+	// Use this for initialization
+	void Start () {
+		
+		if(PC.transform.localScale.x < 0)
+			Speed = -Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
