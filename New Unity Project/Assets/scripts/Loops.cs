8bad632a12598cdd8f17e479c14f68c1808a063c
brennon <bhar9463@gmail.com>
2018-10-24 16:35:58 -0600

    Loops cont.
    Practicing the difference between for and while Loops

--- New Unity Project/Assets/scripts/Loops.cs
+++ New Unity Project/Assets/scripts/Loops.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour {
 
-	
+		public int Num = 40;
 
-	// Use this for initialization
+	// be sure to do "+=" 
 	void Start () {
-		for(int i=0; i < 100; i++){
+		//for(int i=0; i <= 100; i++){
 
-			print(i + "Ants Marching");
-		
+			//print(i + "Ants Marching");
+
+		//}
+		while(Num >= 0){
+			print("Countdown" + Num);
+			Num --;
+
+
 		}
+	
+	
+	
 	}
 	
 	// Update is called once per frame
