aefa6094326d9605ba764d130cfc04e4792a91bb
brennon <bhar9463@gmail.com>
2018-11-05 17:10:44 -0700

    added camera follow
    The camera follows the character now.

---
+++ New Unity Project/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public CharacterMove Jimmy;
+
+	public bool isFollowing;
+
+	// Camera position offset
+
+	public float xOffset;
+	public float yOffset;
+	
+	
+	
+	void Start () {
+		Jimmy = FindObjectOfType<CharacterMove>();
+
+		isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			transform.position = new Vector3(Jimmy.transform.position.x + xOffset, Jimmy.transform.position.y + yOffset, transform.position.z);
+		}
+	}
+}
