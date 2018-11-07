0fcfa9dace8c8e2a73aff8e5c3d1658d1e55b7b5
brennon <bhar9463@gmail.com>
2018-11-07 16:44:14 -0700

    enum (failure)
    fail of the enum process (doesn't exist)

--- New Unity Project/Assets/Homework/Enum.cs
+++ New Unity Project/Assets/Homework/Enum.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Enum : MonoBehaviour {
 
+		enum Inventory: ulong {bread = 1, meat = 10, sword = 1, shield = 2, map = 0, backpack = 12, bow = 3, arrows = 20};
+		
+		// int coins[] = new int{1,20,15,7, 2};
+		
+
 	// Use this for initialization
 	void Start () {
 		
+		print("Bread = " + Inventory.bread);
 	}
 	
 	// Update is called once per frame
