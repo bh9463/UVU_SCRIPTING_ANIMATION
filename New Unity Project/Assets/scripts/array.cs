05f52d19b33bbab33d4fb67b767955212e481e6b
brennon <bhar9463@gmail.com>
2018-11-07 16:25:35 -0700

    array pop quiz
    did the pop quiz and i nailed it... suck it!

---
+++ New Unity Project/Assets/scripts/array.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class array : MonoBehaviour {
+
+		public int[] cheese = new int[12];
+		
+
+	// Use this for initialization
+	void Start () {
+		
+		
+		
+		cheese[0] = 1;
+		cheese[1] = 2;
+		cheese[2] = 3;
+		cheese[3] = 4;
+		cheese[4] = 5;
+		cheese[5] = 6;
+		cheese[6] = 7;
+		cheese[7] = 8;
+		cheese[8] = 9;
+		cheese[9] = 10;
+		cheese[10] = 11;
+		cheese[11] = 12;
+
+		print(cheese[9]);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
