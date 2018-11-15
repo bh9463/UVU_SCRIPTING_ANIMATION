96e6cf2453901edb7749ea29009d9452e98d90f0
brennon <bhar9463@gmail.com>
2018-11-14 17:01:31 -0700

    Added Ammo
    Added Ammo counting

---
+++ New Unity Project/Assets/scripts/Ammo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Ammo : MonoBehaviour{ 	
+
+	public static int AmmoQuantity;
+
+	Text AmmoQuantityText;
+
+	// Use this for initialization
+	void Start () {
+		AmmoQuantityText = GetComponent<Text>();
+
+		AmmoQuantity = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (AmmoQuantity < 0)
+			AmmoQuantity = 0;
+		AmmoQuantityText.text = " " + AmmoQuantity;
+		
+	}
+
+	public static void AddPoints (int PointsToAdd) {
+		AmmoQuantity += PointsToAdd;
+	}
+}
