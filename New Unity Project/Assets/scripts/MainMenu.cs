db6189e58adf3fa358a2dd71e60516de5b7738f4
brennon <bhar9463@gmail.com>
2018-11-26 17:10:20 -0700

    main menu buttons

---
+++ New Unity Project/Assets/scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public int level1ToLoad;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(level1ToLoad);
+	}
+
+	public void Level1Exit(){
+		Application.Quit();
+	}
+}
+
